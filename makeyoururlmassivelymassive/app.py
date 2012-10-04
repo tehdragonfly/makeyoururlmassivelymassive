bdc9d67b616ce943a576bb7a8bfa2c4d4823250d
Olly Parton <mysticdragonfly@hotmail.co.uk>
2012-10-04 15:45:03 +0100

    Stub out views.

--- makeyoururlmassivelymassive/app.py
+++ makeyoururlmassivelymassive/app.py
@@ -3,6 +3,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
+    return "Hello World!"
+
+@app.route("/", methods=["POST"])
+def create():
+    return "Hello World!"
+
+@app.route("/<url>")
+def look_up(url):
     return "Hello World!"
