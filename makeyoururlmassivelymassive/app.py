4fea7bb2a3af03f88052a2e698d664975751b41b
Olly Parton <mysticdragonfly@hotmail.co.uk>
2012-10-08 16:57:10 +0100

    Implemented redirects.

--- makeyoururlmassivelymassive/app.py
+++ makeyoururlmassivelymassive/app.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 from flask import Flask
 from makeyoururlmassivelymassive.db import session, MassiveURL
+from sqlalchemy.orm.exc import NoResultFound
 
 app = Flask(__name__)
 
@@ -16,6 +17,8 @@ def home():
 def create():
     return "Hello World!"
 
-@app.route("/<url>")
-def look_up(url):
-    return "Hello World!"
+@app.route("/<id>")
+def look_up(id):
+    id = id[:32]
+    url = session.query(MassiveURL).filter(MassiveURL.id==id).one()
+    return redirect(url.url)
